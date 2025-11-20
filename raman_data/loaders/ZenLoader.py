d2cc7a1d0cc4d56f87f716e7958d9c3f72f8fc73
Oskar Fulde <oskar.fulde@student.htw-berlin.de>
2025-11-20 14:20:13 +0100

    refactored zenloader

--- raman_data/loaders/ZenLoader.py
+++ raman_data/loaders/ZenLoader.py
@@ -4,7 +4,6 @@ import os, requests
 import pandas as pd
 import numpy as np
 
-
 from raman_data import types
 from raman_data.loaders.ILoader import ILoader
 from raman_data.loaders.LoaderTools import CACHE_DIR, TASK_TYPE, LoaderTools
@@ -16,15 +15,15 @@ class ZenLoader(ILoader):
     """
     
     @staticmethod
-    def __load_10779223(cache_path: str) -> np.ndarray|None:
+    def load_10779223(cache_path: str) -> np.ndarray|None:
         zip_filename = "Raw data.zip"
         
         data_dir = LoaderTools.extract_zip_file_content(os.path.join(cache_path, "10779223", zip_filename), zip_filename)
         
         if data_dir is None:
             return None
-        
-        data_folder_parent = os.path.join(data_dir, "Raw data", "Raw data", "Experimental data from sugar mixtures", "Raw datasets for analyses")
+
+        data_folder_parent = os.path.join(data_dir, "Raw data", "Experimental data from sugar mixtures", "Raw datasets for analyses")
 
         snr = "Low SNR"
         data_folder = os.path.join(data_folder_parent, snr)
@@ -35,22 +34,23 @@ class ZenLoader(ILoader):
         if not os.path.isfile(data_path):
             raise FileNotFoundError(f"Could not find data.pkl in {data_path}")
 
-        # read spectra with pandas
-        spectra = pd.read_pickle(data_path)
+        # read raman_shifts with pandas
+        raman_shifts = pd.read_pickle(data_path).T
 
         # read shifts with pandas
-        shifts_path = os.path.join(data_folder, "spectral_axis.pkl")
-        if not os.path.isfile(shifts_path):
-            raise FileNotFoundError(f"Could not find spectral_axis.pkl in {shifts_path}")
+        spectra_path = os.path.join(data_folder, "spectral_axis.pkl")
 
-        raman_shifts = pd.read_pickle(shifts_path)
+        if not os.path.isfile(spectra_path):
+            raise FileNotFoundError(f"Could not find spectral_axis.pkl in {spectra_path}")
+
+        spectra = pd.read_pickle(spectra_path)
 
         # read gt with pandas
         gt_path = os.path.join(data_folder, "gt_endmembers.pkl")
         if not os.path.isfile(gt_path):
             raise FileNotFoundError(f"Could not find gt_endmembers.pkl in {gt_path}")
 
-        concentrations = pd.read_pickle(gt_path)
+        concentrations = pd.read_pickle(gt_path).T
 
         return raman_shifts, spectra, concentrations
     
@@ -62,9 +62,8 @@ class ZenLoader(ILoader):
     
     @staticmethod
     def load_7644521(cache_path: str) -> np.ndarray|None:
-        raise NotImplementedError
 
-        #data feild names in the mat file
+        #data field names in the mat file
         data_keys = ["COM", "COM_125mM", "ML1_125mM", "ML2_125mM"]
 
         data_path = os.path.join(cache_path, "7644521", "Data.mat")
@@ -80,16 +79,26 @@ class ZenLoader(ILoader):
             return None
         
         #spectra scale
-        spectra = file_content["Calx"]
+        spectra = file_content["Calx"].squeeze()
         raman_shifts = []
+        concentrations = np.array(np.empty)
 
         #raman shift data
         for key in data_keys:
             data_row = file_content[key]
             raman_shifts.append(data_row)
+
+        #TODO Get the concentrations: 
+        #tihs is waht ramanspy dose:
+        #   y = []
+        #   for i, dataset in enumerate(labels):
+        #       #apperently they just add the index of the labe to each label as concentration
+                #COM would 0, COM_125mM would be 1, and so on 
+        #       y.append(np.repeat(i, data[dataset].shape[0]))
+
+        raman_shifts = np.concatenate(raman_shifts).T
         
-        #TODO: Inhomogeneous data array
-        return np.array(raman_shifts), spectra
+        return raman_shifts, spectra, concentrations
     
     
     @staticmethod
@@ -104,8 +113,8 @@ class ZenLoader(ILoader):
 
         df = pd.read_csv(data_path)
         concentrations = df.pop("conc").to_numpy()
-        spectra = np.array(df.columns.values[9:], dtype=int)
-        raman_shifts = df.loc[:, "400":].to_numpy()
+        spectra = np.array(df.columns.values[8:], dtype=int)
+        raman_shifts = df.loc[:, "400":].to_numpy().T
         
         return raman_shifts, spectra, concentrations
 
@@ -117,11 +126,11 @@ class ZenLoader(ILoader):
         "sugar mixtures" : types.datasetInfo(
                                         task_type=TASK_TYPE.Regression, 
                                         id="10779223", 
-                                        loader=__load_10779223),
-        "Volumetric cells" : types.datasetInfo(
-                                        task_type=TASK_TYPE.Classification, 
-                                        id="256329", 
-                                        loader=__load_256329),
+                                        loader=load_10779223),
+        #"Volumetric cells" : types.datasetInfo(
+        #                                task_type=TASK_TYPE.Classification, 
+        #                                id="256329", 
+        #                                loader=load_256329),
         "Wheat lines" : types.datasetInfo(
                                         task_type=TASK_TYPE.Classification, 
                                         id="7644521", 
@@ -130,12 +139,12 @@ class ZenLoader(ILoader):
                                         task_type=TASK_TYPE.Classification, 
                                         id="3572359", 
                                         loader=load_3572359)
-
     }
     
     @staticmethod
     def download_dataset(
-        *dataset_names: str,
+        dataset_name: str,
+        file_name: str | None = None,
         cache_path: Optional[str|None] = None
     ) -> str | None:
         
@@ -144,37 +153,37 @@ class ZenLoader(ILoader):
         cache_path = LoaderTools.get_cache_root(CACHE_DIR.Zenodo)
         cache_path = cache_path if cache_path else ZenLoader.BASE_CACHE_DIR
         
-        dataset_to_download = []
+        #dataset_to_download = []
         
-        if dataset_names:
-            dataset_to_download = dataset_names
-        else:
-            dataset_to_download = ZenLoader.DATASETS.keys()
+        #if dataset_names:
+        #    dataset_to_download = dataset_names
+        #else:
+        #    dataset_to_download = ZenLoader.DATASETS.keys()
         
         
-        for dataset_name in dataset_to_download:
-            if not LoaderTools.is_dataset_available(dataset_name, ZenLoader.DATASETS):
-               print(f"[!] Cannot download {dataset_name} dataset with Zenodo loader")
-               return None
+        #for dataset_name in dataset_to_download:
+        if not LoaderTools.is_dataset_available(dataset_name, ZenLoader.DATASETS):
+           return None
 
-            try:
-                dataset_id = ZenLoader.DATASETS[dataset_name].id
-                file_name = dataset_id + ".zip"
-                url = ZenLoader.BASE_URL.replace("ID", dataset_id)
-                
-                LoaderTools.download(url, cache_path, file_name)
-            except requests.HTTPError as e:
-                print(f"Could not download requested dataset")
-                return None
-            except OSError as e:
-                return None
+        try:
+            dataset_id = ZenLoader.DATASETS[dataset_name].id
+            file_name = dataset_id + ".zip"
+            url = ZenLoader.BASE_URL.replace("ID", dataset_id)
+            
+            LoaderTools.download(url, cache_path, file_name)
+        except requests.HTTPError as e:
+            print(f"Could not download requested dataset")
+            return None
+        except OSError as e:
+            return None
             
         return cache_path
     
     
     @staticmethod
     def load_dataset(
-        *dataset_names: str,
+        dataset_name: str,
+        file_name: str | None = None,
         cache_path: str | None = None
     ) -> np.ndarray | None:
         
@@ -184,32 +193,37 @@ class ZenLoader(ILoader):
         cache_path = cache_path if cache_path else ZenLoader.BASE_CACHE_DIR
         
         
-        datasets_to_load = []
+        #datasets_to_load = []
         
-        loaded_data = {}
+        loaded_datasets = []
         
-        if dataset_names:
-            datasets_to_load = dataset_names
-        else: 
-            datasets_to_load = ZenLoader.DATASETS.keys()
+        #if dataset_name:
+        #    datasets_to_load.append(dataset_name)
+        #else: 
+        #    datasets_to_load = ZenLoader.DATASETS.keys()
             
             
-        for dataset_name in datasets_to_load:
-            if not LoaderTools.is_dataset_available(dataset_name, ZenLoader.DATASETS):
-                return
-            
-            dataset_id = ZenLoader.DATASETS[dataset_name].id
-            
-            zip_file_name = dataset_id + ".zip"
-            zip_file_path = os.path.join(cache_path, zip_file_name)
-            
-            if not os.path.isfile(zip_file_path):
-                ZenLoader.download_dataset(dataset_name, cache_path)
-            
-            if not os.path.isdir(os.path.join(cache_path, dataset_id)):
-                LoaderTools.extract_zip_file_content(zip_file_path, zip_file_name)
-                
-            loaded_data[dataset_name] = ZenLoader.DATASETS[dataset_name].loader(cache_path)
-            
+        #for dataset_name in datasets_to_load:
+        if not LoaderTools.is_dataset_available(dataset_name, ZenLoader.DATASETS):
+            return None
         
-        #TODO: convert data to RamanDatasets type
+        dataset_id = ZenLoader.DATASETS[dataset_name].id
+        
+        zip_file_name = dataset_id + ".zip"
+        zip_file_path = os.path.join(cache_path, zip_file_name)
+        
+        if not os.path.isfile(zip_file_path):
+            ZenLoader.download_dataset(dataset_name, cache_path)
+        
+        if not os.path.isdir(os.path.join(cache_path, dataset_id)):
+            LoaderTools.extract_zip_file_content(zip_file_path, zip_file_name)
+        
+        raman_shifts, spectra, concentrations = ZenLoader.DATASETS[dataset_name].loader(cache_path)
+
+        dataset = types.RamanDataset(data=raman_shifts, 
+                                     target=concentrations, 
+                                     metadata={"spectra":spectra})
+        
+        return dataset
+        
+        
