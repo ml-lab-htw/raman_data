7b7a79bdf27a09dacd67558dd27ab41a010f5d59
Oskar Fulde <oskar.fulde@student.htw-berlin.de>
2025-11-24 12:54:38 +0100

    data extraction for chlange/SubstrateMixRaman dataset from Hugloader

--- raman_data/loaders/HugLoader.py
+++ raman_data/loaders/HugLoader.py
@@ -1,8 +1,12 @@
 from typing import Optional
 
-from datasets import load_dataset
+import datasets
+#from datasets import load_dataset
 from numpy import ndarray
+import pandas as pd
+import numpy as np
 
+from raman_data import types
 from raman_data.loaders.ILoader import ILoader
 from raman_data.loaders.LoaderTools import CACHE_DIR, TASK_TYPE, LoaderTools
 
@@ -12,6 +16,27 @@ class HugLoader(ILoader):
     """
     DATASETS = {
         "chlange/SubstrateMixRaman": TASK_TYPE.Regression
+    }
+
+
+    def load_substarteMix(data: pd.DataFrame) -> np.ndarray|None:
+
+        df = pd.concat([pd.DataFrame(data["train"]),pd.DataFrame(data["test"]) ,pd.DataFrame(data["validation"])], ignore_index=True)
+            
+        end_data_index = len(df.columns.values)-8
+
+        concentrations = df.loc[:, "Glucose":].to_numpy()
+        spectra = np.array(df.columns.values[:end_data_index])
+        raman_shifts = df.loc[:, :"3384.7"].to_numpy().T
+
+        return raman_shifts, spectra, concentrations
+
+
+    DATASETS_INFO = {
+        "chlange/SubstrateMixRaman" : types.datasetInfo(
+                                                    task_type=TASK_TYPE.Regression,
+                                                    id=None,
+                                                    loader=load_substarteMix)
     }
 
 
@@ -30,7 +55,7 @@ class HugLoader(ILoader):
         cache_path = LoaderTools.get_cache_root(CACHE_DIR.HuggingFace)
 
         print(f"Downloading HuggingFace dataset: {dataset_name}")
-        load_dataset(
+        datasets.load_dataset(
             path=dataset_name,
             data_files=file_name,
             cache_dir=cache_path
@@ -55,18 +80,16 @@ class HugLoader(ILoader):
         if not (cache_path is None):
             LoaderTools.set_cache_root(cache_path, CACHE_DIR.HuggingFace)
         cache_path = LoaderTools.get_cache_root(CACHE_DIR.HuggingFace)
-        
+
         print(f"Loading HuggingFace dataset from " \
               f"{cache_path if cache_path else 'default folder (~/.cache/huggingface)'}")
 
-        df = load_dataset(
+        dataDict = datasets.load_dataset(
             path=dataset_name,
-            data_files=file_name,
             cache_dir=cache_path
         )
-        nd = df.with_format("numpy")
 
-        return nd
+        return HugLoader.DATASETS_INFO[dataset_name].loader(dataDict)
 
 
     @staticmethod
