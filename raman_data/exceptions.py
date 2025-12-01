b68b28ab0ca781146ea012a7c07fb3e943b8e9b5
Oskar Fulde <oskar.fulde@student.htw-berlin.de>
2025-12-01 16:48:56 +0100

    New CorruptedZipFile Exceptio - a Exception thrown if a zip file is corrupted

--- raman_data/exceptions.py
+++ raman_data/exceptions.py
@@ -14,3 +14,18 @@ class ChecksumError(Exception):
     def __str__(self) -> str:
         return f"Expected: {self.expected_checksum} but got {self.actual_checksum}"
     
+
+class CorruptedZipFileError(Exception):
+    def __init__(
+        self, 
+        zip_file_path: Optional[str] = None,
+        zip_file_name: Optional[str] = None
+    ) -> None:
+        
+        super().__init__()
+        self.zip_file_path = zip_file_path
+        self.zip_file_name = zip_file_name
+        
+    def __str__(self) -> str:
+         return f"The file {self.zip_file_name} at {self.zip_file_path} seams to be corrupted or in a other way not usable!"
+    
