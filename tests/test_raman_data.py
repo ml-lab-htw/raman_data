3882a463687fa96590deee67c8e5955619c5c3e1
Robert Koch <Robert.Koch@Student.HTW-Berlin.de>
2025-12-01 09:55:45 +0100

    Temporarly excluded external datasets from tests
    Comment out classification tasks in test data.

--- tests/test_raman_data.py
+++ tests/test_raman_data.py
@@ -10,10 +10,10 @@ __DATASETS = {
     # "sergioalejandrod/raman-spectroscopy": TASK_TYPE.Classification,
     # "andriitrelin/cells-raman-spectra": TASK_TYPE.Classification,
     "chlange/SubstrateMixRaman": TASK_TYPE.Regression,
-    "MIND-Lab_covid+pd_ad_bundle": TASK_TYPE.Classification,
-    "csho33_bacteria_id": TASK_TYPE.Classification,
+    #"MIND-Lab_covid+pd_ad_bundle": TASK_TYPE.Classification,
+    #"csho33_bacteria_id": TASK_TYPE.Classification,
     # "mendeley_surface-enhanced-raman": TASK_TYPE.Classification,
-    "dtu_raman-spectrum-matching": TASK_TYPE.Classification,
+    #"dtu_raman-spectrum-matching": TASK_TYPE.Classification,
     "sugar mixtures": TASK_TYPE.Regression,
     "Wheat lines" : TASK_TYPE.Classification,
     "Adenine" : TASK_TYPE.Classification
