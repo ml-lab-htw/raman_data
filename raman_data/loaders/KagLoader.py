75e705ac9bcf14409f6292335bb3dda3dbef6381
Oskar Fulde <oskar.fulde@student.htw-berlin.de>
2025-12-01 15:51:51 +0100

    New Datasets for KaggLoader - implemnted multiple missing datasets - style changes to be in line with the other loaders

--- raman_data/loaders/KagLoader.py
+++ raman_data/loaders/KagLoader.py
@@ -1,9 +1,12 @@
+import os
+
 from typing import Optional
 
-from kagglehub import load_dataset, dataset_download
+from kagglehub import dataset_load, dataset_download
 from kagglehub import KaggleDatasetAdapter
-from numpy import ndarray
+import numpy as np
 
+from raman_data import types
 from raman_data.types import DatasetInfo
 from raman_data.loaders.ILoader import ILoader
 from raman_data.loaders.LoaderTools import CACHE_DIR, TASK_TYPE, LoaderTools
@@ -12,20 +15,99 @@ class KagLoader(ILoader):
     """
     A static class specified in providing datasets hosted on Kaggle.
     """
+
+    @staticmethod
+    def load_diabetes(id: str) -> np.ndarray|None:
+        
+        file_handle = "codina/raman-spectroscopy-of-diabetes"
+
+        df = dataset_load(
+            adapter=KaggleDatasetAdapter.PANDAS,
+            handle=file_handle,
+            path= id + ".csv"
+        )
+
+        if id == "AGEs":
+            spectra = df.loc[0, "Var802":].to_numpy()
+            raman_shifts = df.loc[1:, "Var802":].to_numpy().T
+            concentration = df.loc[1:, "AGEsID"].to_numpy()
+        else:
+            spectra = df.loc[0, "Var2":].to_numpy()
+            raman_shifts = df.loc[1:, "Var2":].to_numpy().T
+            concentration = df.loc[1:, "has_DM2"].to_numpy()
+
+        return raman_shifts, spectra, concentration
+
+    
+    @staticmethod
+    def load_sergioalejandrod(id:str) -> np.ndarray|None:
+        file_handle = "sergioalejandrod/raman-spectroscopy"
+        header = ["Gly, 40 mM", "Leu, 40 mM", "Phe, 40 mM", "Trp, 40 mM"]
+
+
+        df = dataset_load(
+            adapter=KaggleDatasetAdapter.PANDAS,
+            handle=file_handle,
+            path="AminoAcids_40mM.xlsx",
+            pandas_kwargs={"sheet_name": f"Sheet{id}"}
+        )
+
+        spectra = df.loc[:, header[(int(id)-1)]].to_numpy()
+        raman_shifts = df.loc[1:, 4.5:].to_numpy().T
+        concentration = np.array(df.columns.values[2:], dtype=float)
+
+        return raman_shifts, spectra, concentration
+    
+    
+    @staticmethod
+    def load_andriitrelin():
+        raise NotImplementedError
+
     DATASETS = {
-        "codina/raman-spectroscopy-of-diabetes": DatasetInfo(
-            task_type=TASK_TYPE.Classification,
-            id="1",
-            load=...),
-        "sergioalejandrod/raman-spectroscopy": DatasetInfo(
-            task_type=TASK_TYPE.Classification,
-            id="2",
-            load=...),
-        "andriitrelin/cells-raman-spectra": DatasetInfo(
-            task_type=TASK_TYPE.Classification,
-            id="3",
-            load=...)
+        "codina/diabetes/AGEs" : DatasetInfo(
+                                                task_type=TASK_TYPE.Classification,
+                                                id="AGEs",
+                                                loader=load_diabetes),
+        "codina/diabetes/earLobe" : DatasetInfo(
+                                                task_type=TASK_TYPE.Classification,
+                                                id="earLobe",
+                                                loader=load_diabetes),
+        "codina/diabetes/innerArm" : DatasetInfo(
+                                                task_type=TASK_TYPE.Classification,
+                                                id="innerArm",
+                                                loader=load_diabetes),
+        "codina/diabetes/thumbNail" : DatasetInfo(
+                                                task_type=TASK_TYPE.Classification,
+                                                id="thumbNail",
+                                                loader=load_diabetes),
+        "codina/diabetes/vein" : DatasetInfo(
+                                                task_type=TASK_TYPE.Classification,
+                                                id="vein",
+                                                loader=load_diabetes),
+
+        "sergioalejandrod/AminoAcids/glycine" : DatasetInfo(
+                                                task_type=TASK_TYPE.Classification,
+                                                id="1",
+                                                loader=load_sergioalejandrod),
+        "sergioalejandrod/AminoAcids/leucine" : DatasetInfo(
+                                                task_type=TASK_TYPE.Classification,
+                                                id="2",
+                                                loader=load_sergioalejandrod),
+        "sergioalejandrod/AminoAcids/phenylalanine" : DatasetInfo(
+                                                task_type=TASK_TYPE.Classification,
+                                                id="3",
+                                                loader=load_sergioalejandrod),
+        "sergioalejandrod/AminoAcids/tryptophan" : DatasetInfo(
+                                                task_type=TASK_TYPE.Classification,
+                                                id="4",
+                                                loader=load_sergioalejandrod),
+
+        #"andriitrelin/cells-raman-spectra" : types.datasetInfo(
+        #                                                    task_type=TASK_TYPE.Classification,
+        #                                                    id=None,
+        #                                                    loader=load_andriitrelin)
     }
+
 
     @staticmethod
     def download_dataset(
@@ -55,7 +137,7 @@ class KagLoader(ILoader):
         dataset_name: str,
         file_name: str,
         cache_path: Optional[str] = None
-    ) -> ndarray | None:
+    ) -> np.ndarray | None:
         if not LoaderTools.is_dataset_available(dataset_name, KagLoader.DATASETS):
             print(f"[!] Cannot load {dataset_name} dataset with Kaggle loader")
             return
@@ -66,14 +148,9 @@ class KagLoader(ILoader):
         print(f"Loading Kaggle dataset from " \
               f"{cache_path if cache_path else 'default folder (~/.cache/kagglehub)'}")
 
-        df = load_dataset(
-            adapter=KaggleDatasetAdapter.PANDAS,
-            handle=dataset_name,
-            path=file_name,
-        )
-        nd = df.to_numpy()
+        dataset_id = KagLoader.DATASETS[dataset_name].id
 
-        return nd
+        return KagLoader.DATASETS[dataset_name].loader(dataset_id)
 
 
     @staticmethod
