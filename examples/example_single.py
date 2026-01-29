f118c71c5657fe3fad9b24d709b1f3725c9ed814
Mario Koddenbrock <koddenbrock@gmail.com>
2026-01-29 11:11:18 +0100

    Update example_single.py to use mind_covid dataset instead of adenine_cAg

--- examples/example_single.py
+++ examples/example_single.py
@@ -1,7 +1,7 @@
 from examples.utils import plot_samples
 from raman_data import raman_data
 
-dataset_name = "adenine_cAg"
+dataset_name = "mind_covid"
 dataset = raman_data(dataset_name)
 
 plot_samples(dataset)
