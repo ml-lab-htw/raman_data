4e0fa2171195cb6ce454a50b078a2d96168a4e6e
Mario Koddenbrock <koddenbrock@gmail.com>
2026-01-29 22:25:13 +0100

    Update example_single.py to use covid19_serum dataset instead of mind_covid

--- examples/example_single.py
+++ examples/example_single.py
@@ -1,7 +1,7 @@
 from examples.utils import plot_samples
 from raman_data import raman_data
 
-dataset_name = "mind_covid"
+dataset_name = "covid19_serum"
 dataset = raman_data(dataset_name)
 
 plot_samples(dataset)
