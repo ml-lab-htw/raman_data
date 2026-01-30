0a066b86971217d1fabd901d4a154dbddcfdeff1
Mario Koddenbrock <koddenbrock@gmail.com>
2026-01-30 09:53:51 +0100

    Add encode_labels utility function for label encoding with target names

--- raman_data/loaders/utils.py
+++ raman_data/loaders/utils.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+from pandas import Series
+from sklearn.preprocessing import LabelEncoder
 
 
 def is_wavenumber(col: str) -> bool:
@@ -7,3 +11,10 @@ def is_wavenumber(col: str) -> bool:
         return True
     except ValueError:
         return False
+
+
+def encode_labels(targets: Series) -> tuple[Any, Any]:
+    le = LabelEncoder()
+    encoded_targets = le.fit_transform(targets)
+    target_names = le.classes_
+    return encoded_targets, target_names
