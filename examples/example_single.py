8be65eb4782cd0acacf41ba39ca50b6c339f7f67
Mario Koddenbrock <koddenbrock@gmail.com>
2026-02-06 15:09:02 +0100

    Refactor dataset selection in example_single.py to filter for 'sop_spectral_library' datasets

--- examples/example_single.py
+++ examples/example_single.py
@@ -1,14 +1,14 @@
 from examples.utils import plot_samples
 from raman_data import raman_data, TASK_TYPE
 
-# dataset_name = "sop_spectral_library_baseline_corrected"
+# dataset_name = "knowitall_organics_raw"
 # dataset = raman_data(dataset_name)
 # plot_samples(dataset)
 
 
-regression_datasets = raman_data(task_type=TASK_TYPE.Regression)
-acid_species_datasets = [dataset for dataset in regression_datasets if "microgel_size" in dataset]
+datasets = raman_data(task_type=None)
+filtered_datasets = [dataset for dataset in datasets if "sop_spectral_library" in dataset]
 
-for dataset_name in acid_species_datasets:
+for dataset_name in filtered_datasets:
     dataset = raman_data(dataset_name)
     plot_samples(dataset)
