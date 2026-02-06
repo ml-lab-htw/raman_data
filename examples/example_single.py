ab169af11afa8f55dcada4ae2f53e73e507bb052
Mario Koddenbrock <koddenbrock@gmail.com>
2026-02-06 19:57:11 +0100

    Update dataset filtering criteria to include only diabetes skin datasets

--- examples/example_single.py
+++ examples/example_single.py
@@ -7,7 +7,7 @@ from raman_data import raman_data, TASK_TYPE
 
 
 datasets = raman_data(task_type=None)
-filtered_datasets = [dataset for dataset in datasets if "sop_spectral_library" in dataset]
+filtered_datasets = [dataset for dataset in datasets if "diabetes_skin_" in dataset]
 
 for dataset_name in filtered_datasets:
     dataset = raman_data(dataset_name)
