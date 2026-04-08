308a47ca2892760b86ef7e0f5b293e039ae41ac5
Mario Koddenbrock <koddenbrock@gmail.com>
2026-04-08 21:03:06 +0200

    Update dataset filtering to focus on bioprocess substrates and remove unused class count logic

--- examples/example_single.py
+++ examples/example_single.py
@@ -8,21 +8,21 @@ from raman_data import raman_data
 
 
 datasets = raman_data(task_type=None)
-# datasets = [dataset for dataset in datasets if "diabetes_skin_ear_lobe" in dataset]
+datasets = [dataset for dataset in datasets if "bioprocess_substrates" in dataset]
 
 for dataset_name in datasets:
     dataset = raman_data(dataset_name)
 
 
-    class_counts = Counter(dataset.targets)
+    # class_counts = Counter(dataset.targets)
+    #
+    # filtered_classes = {class_name: count for class_name, count in class_counts.items() if count >= 9}
+    # sorted_classes = sorted(filtered_classes.items(), key=lambda x: x[1])
+    #
+    # if len(class_counts) != len(filtered_classes):
+    #     print(f"Dataset: {dataset_name}")
+    #     print(f"  Number of classes before filtering: {len(class_counts)}")
+    #     print(f"  Number of classes after filtering: {len(filtered_classes)}")
 
-    filtered_classes = {class_name: count for class_name, count in class_counts.items() if count >= 9}
-    sorted_classes = sorted(filtered_classes.items(), key=lambda x: x[1])
 
-    if len(class_counts) != len(filtered_classes):
-        print(f"Dataset: {dataset_name}")
-        print(f"  Number of classes before filtering: {len(class_counts)}")
-        print(f"  Number of classes after filtering: {len(filtered_classes)}")
-
-
-    # plot_samples(dataset)
+    plot_samples(dataset)
